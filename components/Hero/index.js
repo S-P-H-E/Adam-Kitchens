ccd2c93eb86b91cb903541b22b812affc28a4f8a
Siphesihle Mbuyisa <siphesihlembuyisa@Sphes-MacBook-Air.local>
2023-08-03 13:21:11 +0200

    &apos;

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -8,7 +8,7 @@ export default function Hero({name, title}){
         <div className="flex flex-col h-screen">
           <Nav name={name} />
           <div className="flex flex-col justify-center items-center mb-20 h-full bg-gradient-to-t from-[var(--highlight)] via-white to-white from-10% via-70% to-90%">
-            <p className="text-base md:text-lg border-[var(--text)] m-4 px-4 py-1 rounded-full">Hi I'm {name}</p>
+            <p className="text-base md:text-lg border-[var(--text)] m-4 px-4 py-1 rounded-full">Hi I&apos;m {name}</p>
             <h1 className="text-5xl sm:text-7xl text-center font-bold md:w-[800px]">{title}</h1>
             <AI />
             <div className="flex flex-col gap-5">
